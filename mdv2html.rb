2bc076ca0506ee0023e7fa29018083011a07ed19
ujihisa <ujihisa@gmail.com>
2009-06-04 22:56:05 -0700

    import mdv2html.rb (includes an implementation and the spec)

---
+++ mdv2html.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+# vim: fileencoding=utf-8 foldmethod=marker
+require 'rubygems'
+require 'markdown' # peg-markdown
+
+def mdv2html(text)
+  text = text.lines.to_a[1..-1].join # Just ignore the first line
+  body = Markdown.new(text).to_html
+  body.gsub!(%r!<code>\|(.*?)\|</code>!, '<kbd>\1</kbd>')
+  body.gsub!(%r!^<p>Author: (.*?)</p>$!, '<p><author>\1</author></p>') # FIXME: It should apply ONLY for the last line
+  body
+end
+
+fixture =
+<<EOF # {{{
+# Hack #22: Ultra Super Great Vim Plugin | lv2 2009-06-06
+
+## PROBLEM
+blah blah blah
+
+## SOLUTION
+use [blogger.vim](http://www.vim.org/scripts/script.php?script_id=2638)
+with
+
+the key mapping `|j|`.
+
+## DISCUSSION
+blah blah blah
+
+    nnoremap j :<C-u>1000sl<Cr>
+    nnoremap k :<C-u>1000sl<Cr>
+
+blah blah blah. hara y y hara y?
+
+Author: ujihisa
+EOF
+# }}}
+
+case $0
+when __FILE__
+  puts mdv2html(fixture)
+when /spec$/
+  describe 'mdv2html' do
+    it 'encodes mdv text to html text' do
+      html =
+      <<-EOF.gsub(/^\s+\|/, '').chomp # {{{
+      |<h2>PROBLEM</h2>
+      |
+      |<p>blah blah blah</p>
+      |
+      |<h2>SOLUTION</h2>
+      |
+      |<p>use <a href="http://www.vim.org/scripts/script.php?script_id=2638">blogger.vim</a>
+      |with</p>
+      |
+      |<p>the key mapping <kbd>j</kbd>.</p>
+      |
+      |<h2>DISCUSSION</h2>
+      |
+      |<p>blah blah blah</p>
+      |
+      |<pre><code>nnoremap j :&lt;C-u&gt;1000sl&lt;Cr&gt;
+      |nnoremap k :&lt;C-u&gt;1000sl&lt;Cr&gt;
+      |</code></pre>
+      |
+      |<p>blah blah blah. hara y y hara y?</p>
+      |
+      |<p><author>ujihisa</author></p>
+      EOF
+      # }}}
+      mdv2html(fixture).should be_instance_of(String)
+      mdv2html(fixture).should == html
+    end
+  end
+end
